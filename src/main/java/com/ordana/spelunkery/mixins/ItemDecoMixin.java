a62cbda1a876a0a47b659fd3c8addf6d7672060d
The Birthday Coyote :] <constachugga@gmail.com>
2025-08-13 14:35:27 -0700

    uhhh

--- src/main/java/com/ordana/spelunkery/mixins/ItemDecoMixin.java
+++ src/main/java/com/ordana/spelunkery/mixins/ItemDecoMixin.java
@@ -7,13 +7,18 @@ import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.util.Mth;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.component.BlockItemStateProperties;
 import net.minecraft.world.item.component.BundleContents;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.neoforged.neoforge.client.extensions.IGuiGraphicsExtension;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Objects;
 
 @Mixin(GuiGraphics.class)
 public abstract class ItemDecoMixin implements IGuiGraphicsExtension
@@ -32,38 +37,60 @@ public abstract class ItemDecoMixin implements IGuiGraphicsExtension
 		@Local(argsOnly=true, ordinal=1) int y
 	)
 	{
-		if (original)
+		if (stack.is(Items.BUNDLE))
 		{
-			if (stack.is(Items.BUNDLE))
+			final var C_PARTIAL = 0xFF_7F7FFF;
+			final var C_FULL = 0xFF_FF7F7F;
+			
+			final var contents = stack.getOrDefault(DataComponents.BUNDLE_CONTENTS, BundleContents.EMPTY);
+			final var fac = contents.weight().doubleValue();
+			
+			if (fac > 0.0)
 			{
-				final var C_PARTIAL = 0xFF_7F7FFF;
-				final var C_FULL = 0xFF_FF7F7F;
-				
-				final var contents = stack.getOrDefault(DataComponents.BUNDLE_CONTENTS, BundleContents.EMPTY);
-				final var fac = contents.weight().doubleValue();
-				
-				if (fac > 0.0)
-				{
-					final var c = Mth.equal(fac, 1.0) ? C_FULL : C_PARTIAL;
-					fill(RenderType.GUI_OVERLAY, x, y, x + 16, y + (int)Math.round(fac * 16), (0xFF<<24)|c);
-				}
-				
+				final var c = Mth.equal(fac, 1.0) ? C_FULL : C_PARTIAL;
+				fill(RenderType.GUI_OVERLAY, x, y, x + 8, y + (int)Math.round(fac * 16), (0xFF<<24)|c);
 			}
-			else
-			{
-				final int valueMax = stack.getOrDefault(DataComponents.MAX_DAMAGE, 0);
-				final int value    = stack.getOrDefault(DataComponents.DAMAGE, 0);
-				
-				if (valueMax > 0)
-				{
-					final var fac = (double)value / (double)valueMax;
-					final var c = stack.getBarColor();
-					fill(RenderType.GUI_OVERLAY, x, y, x + 16, y + 16 - (int)Math.round(fac * 16), (0xFF<<24)|c);
-				}
-			}
-		
 			
 		}
+		else if (stack.getItem() instanceof BlockItem bi)
+		{
+			if (bi == Items.BEE_NEST || bi == Items.BEEHIVE)
+			{
+				final var bs = stack.getOrDefault(DataComponents.BLOCK_STATE, BlockItemStateProperties.EMPTY);
+				final int honey = Objects.requireNonNullElse(bs.get(BlockStateProperties.LEVEL_HONEY), 0);
+				if (honey > 0)
+				{
+					final var HR_MIN = 0xac;
+					final var HG_MIN = 0x32;
+					final var HB_MIN = 0x32;
+					final var HR_MAX = 0xfb;
+					final var HG_MAX = 0xf2;
+					final var HB_MAX = 0x36;
+					
+					final var fac = (double)Mth.clamp(honey, 0, 5) / 5.0;
+					final var ifac = (int)Math.round(fac * 255.0);
+					final var r = (((HR_MAX-HR_MIN) * ifac + 0xFF) >> 8) + HR_MIN;
+					final var g = (((HG_MAX-HG_MIN) * ifac + 0xFF) >> 8) + HG_MIN;
+					final var b = (((HB_MAX-HB_MIN) * ifac + 0xFF) >> 8) + HB_MIN;
+					final var c = ((r&0xFF)<<16)|((g&0xFF)<<8)|(b&0xFF)|(0xFF<<24);
+					fill(RenderType.GUI_OVERLAY, x, y, x + 8, y + (int)Math.round(fac * 16), c);
+				}
+			}
+		}
+		else
+		{
+			final int valueMax = stack.getOrDefault(DataComponents.MAX_DAMAGE, 0);
+			final int value    = stack.getOrDefault(DataComponents.DAMAGE, 0);
+			
+			if (valueMax > 0)
+			{
+				final var fac = (double)value / (double)valueMax;
+				final var c = stack.getBarColor();
+				fill(RenderType.GUI_OVERLAY, x, y, x + 8, y + 16 - (int)Math.round(fac * 16), (0xFF<<24)|c);
+			}
+		}
+		
+		
 		return false;
 	}
 	
--- src/main/resources/assets/spelunkery/models/item_slot/test.lua
+++ src/main/resources/assets/spelunkery/models/item_slot/test.lua
@@ -5,6 +5,7 @@ for i = 1, fc do
 end
 
 return {
+	at = { 0, 0 },
 	source = 'spelunkery:slot/bar/remaining_durability',
 	empty = 0,
 	full = fc - 1,
