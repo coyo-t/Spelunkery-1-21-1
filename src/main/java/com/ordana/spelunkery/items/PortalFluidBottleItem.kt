034515af1bef87a643c6062e28bf71746ceb4a27
The Birthday Coyote :] <constachugga@gmail.com>
2025-08-12 20:22:06 -0700

    uhhh

--- src/main/java/com/ordana/spelunkery/items/PortalFluidBottleItem.kt
+++ src/main/java/com/ordana/spelunkery/items/PortalFluidBottleItem.kt
@@ -4,17 +4,17 @@ import com.ordana.spelunkery.reg.GameRulez
 import com.ordana.spelunkery.utils.LevelHelper
 import net.minecraft.ChatFormatting
 import net.minecraft.advancements.CriteriaTriggers
-import net.minecraft.core.component.DataComponents
 import net.minecraft.core.component.DataComponents.LODESTONE_TRACKER
 import net.minecraft.network.chat.Component
 import net.minecraft.server.level.ServerPlayer
 import net.minecraft.sounds.SoundEvents
 import net.minecraft.stats.Stats
+import net.minecraft.world.effect.MobEffectInstance
+import net.minecraft.world.effect.MobEffects
 import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.HoneyBottleItem
 import net.minecraft.world.item.ItemStack
-import net.minecraft.world.item.ItemUtils
 import net.minecraft.world.item.Items
 import net.minecraft.world.item.TooltipFlag
 import net.minecraft.world.level.Level
@@ -37,49 +37,70 @@ class PortalFluidBottleItem(properties: Properties) : HoneyBottleItem(properties
 		tooltipFlag: TooltipFlag
 	)
 	{
-		if (tooltipFlag.hasShiftDown())
-		{
-			stack.get(LODESTONE_TRACKER)?.target?.ifPresent {
-				val co = it.pos
-				val dim = it.dimension
-				val cal = Component.translatable(
-					"tooltip.spelunkery.portal_fluid_target",
-					-co.z,
-					+co.x,
-					+co.y,
-					dim.toString(),
-				)
-				cal.withStyle(ChatFormatting.GRAY)
-				tooltipComponents += cal
-			}
+		stack.get(LODESTONE_TRACKER)?.target?.ifPresent {
+			val co = it.pos
+			val dim = it.dimension
+			val cal = Component.translatable(
+				"tooltip.spelunkery.portal_fluid_target",
+				-co.z,
+				+co.x,
+				+co.y,
+				Component.translatable(dim.location().toLanguageKey("dimension"))
+			)
+
+			cal.withStyle(ChatFormatting.GRAY)
+			tooltipComponents += cal
 		}
+
 		super.appendHoverText(stack, context, tooltipComponents, tooltipFlag)
 	}
 
-	override fun finishUsingItem(stack: ItemStack, level: Level, livingEntity: LivingEntity): ItemStack
+	override fun finishUsingItem(stack: ItemStack, level: Level, user: LivingEntity): ItemStack
 	{
-		if (livingEntity is Player)
+		val eatRes = when (val fp = stack.getFoodProperties(user))
 		{
-			livingEntity.setItemInHand(
-				livingEntity.usedItemHand,
-				ItemUtils.createFilledResult(stack, livingEntity, Items.GLASS_BOTTLE.defaultInstance)
-			)
+			null -> stack
+			else -> user.eat(level, stack, fp)
 		}
-		if (livingEntity is ServerPlayer && level.gameRules.getBoolean(GameRulez.BADLANDS_CHUGS_PORTALS))
-		{
-			CriteriaTriggers.CONSUME_ITEM.trigger(livingEntity, stack)
-			livingEntity.awardStat(Stats.ITEM_USED.get(this))
 
-			val uhh = stack.get(LODESTONE_TRACKER)
-			if (uhh == null)
-			{
-				LevelHelper.teleportToSpawnPosition(livingEntity)
-			}
-			else
-			{
-				uhh.target.ifPresent { LevelHelper.teleportToAnchorPosition(livingEntity, it) }
-			}
+		if (eatRes.isEmpty)
+		{
+			return ItemStack(Items.GLASS_BOTTLE)
 		}
-		return stack
+		else
+		{
+			if (user is Player)
+			{
+				user.addEffect(MobEffectInstance(MobEffects.CONFUSION, 4 * level.tickRateManager().tickrate().toInt(), 6))
+				if (!user.hasInfiniteMaterials())
+				{
+					val itemstack = ItemStack(Items.GLASS_BOTTLE)
+					if (!user.getInventory().add(itemstack))
+					{
+						user.drop(itemstack, false)
+					}
+				}
+
+				if (level.gameRules.getBoolean(GameRulez.BADLANDS_CHUGS_PORTALS))
+				{
+					if (user is ServerPlayer)
+					{
+						CriteriaTriggers.CONSUME_ITEM.trigger(user, stack)
+						user.awardStat(Stats.ITEM_USED.get(this))
+
+						val uhh = stack.get(LODESTONE_TRACKER)
+						if (uhh == null)
+						{
+							LevelHelper.teleportToSpawnPosition(user)
+						}
+						else
+						{
+							uhh.target.ifPresent { LevelHelper.teleportToAnchorPosition(user, it) }
+						}
+					}
+				}
+			}
+			return eatRes
+		}
 	}
 }
